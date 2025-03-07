fb533e1c6fed29694ca829334cf4855906c25f44
aasif-iqbal <aasif.iqbal3000@gmail.com>
2025-03-07 22:19:36 +0530

    Initial commit

---
+++ test/microservice-notification.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MicroserviceNotification from '../lib/microservice-notification-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/microservice-notification-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MicroserviceNotification.MicroserviceNotificationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
